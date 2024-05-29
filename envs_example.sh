d248d87a67fa77d59f8623a69f1f5ed809fca292
Alexey Sazonov <ezhische@gmail.com>
2024-05-30 01:55:05 +0300

    add envs exapmle

---
+++ envs_example.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+export EVOTOR_API_URL="https://api.evotor.ru/stores"
+export GOOGLE_KEY_FILE_NAME="./sa.json"
+export EVOTOR_STORE_ID="XXXXXXX-XXXX-XXXX-80AF-XXXXXXXXXX"
+export EVOTOR_API_TOKEN="27186d81-XXXX-XXXX-XXXX-13098bc69827"
+export BARTENDERS_LIST='["Ваня", "Влад", "Леша", "Марат", "Алиса", "Илья", "Олег", "Даша","Георгий"]'
+export EXCEPT_GROUPS='["Cнеки","Гриль"]'
+export PRIME_MIN=10000
